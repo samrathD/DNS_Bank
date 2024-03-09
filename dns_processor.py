6ad39bec387352da833f2c6d330f57a5cc98ffed
samrathD <samrath2004@gmail.com>
2024-03-09 14:12:55 -0800

    receive transaction data

--- dns_processor.py
+++ dns_processor.py
@@ -1,2 +1,39 @@
 import dns_network_api as dns
+import bank_interact as bank
+import re
+
+PORT = "105"
+print(dns.network_check(PORT))
+def process():
+    while dns.network_status() == "Status: Server Is Online":
+            if dns.network_check(PORT)!="Check: {}: 0".format(PORT) :
+                transcation = dns.network_request(PORT)
+                print(transcation)
+                pattern = r'\{([^}]*)\}'
+                matches = re.findall(pattern,transcation)
+
+                if matches:
+                     card_data = matches[0]
+                     price = matches[1]
+                     vendor = matches[2]
+                     card_data=card_data.split("_")
+                     id = card_data[0]
+                     code = card_data[1]
+                     print(id)
+                     print(code)
+                     print(price)
+                     print(vendor)
+                else:
+                     print("no Match")
+                
+                
+                break
+            else:
+                print("No good")
+                print(dns.network_check(PORT))
+
+                break
+        
+process()
+print(dns.network_check(PORT))
 
