b9681357656c9957c38305a8b939e0b55d8578b5
samrathD <samrath2004@gmail.com>
2024-03-09 14:40:59 -0800

    new

--- dns_processor.py
+++ dns_processor.py
@@ -1,39 +1,46 @@
 import dns_network_api as dns
+import dns_bank_api as dns_bank
 import bank_interact as bank
 import re
+import time
 
+
+TEMP_VENDOR = "12216819421512709190"
 PORT = "105"
-print(dns.network_check(PORT))
-def process():
-    while dns.network_status() == "Status: Server Is Online":
-            if dns.network_check(PORT)!="Check: {}: 0".format(PORT) :
-                transcation = dns.network_request(PORT)
-                print(transcation)
-                pattern = r'\{([^}]*)\}'
-                matches = re.findall(pattern,transcation)
+# print(dns.network_check(PORT))
+# def process():
+#     while dns.network_status() == "Status: Server Is Online":
+#             if dns.network_check(PORT)!="Check: {}: 0".format(PORT) :
+#                 transcation = dns.network_request(PORT)
+#                 print(transcation)
+#                 pattern = r'\{([^}]*)\}'
+#                 matches = re.findall(pattern,transcation)
 
-                if matches:
-                     card_data = matches[0]
-                     price = matches[1]
-                     vendor = matches[2]
-                     card_data=card_data.split("_")
-                     id = card_data[0]
-                     code = card_data[1]
-                     print(id)
-                     print(code)
-                     print(price)
-                     print(vendor)
-                else:
-                     print("no Match")
+#                 if matches:
+#                      card_data = matches[0]
+#                      price = matches[1]
+#                      vendor = matches[2]
+#                      card_data=card_data.split("_")
+#                      id = card_data[0]
+#                      code = card_data[1]
+#                      print(id)
+#                      print(code)
+#                      print(price)
+#                      print(vendor)
+#                 else:
+#                      print("no Match")
                 
-                
-                break
-            else:
-                print("No good")
-                print(dns.network_check(PORT))
+#                 bank.process_bank_transfer(id, TEMP_VENDOR, code, price)
+#                 time.sleep(5)
+#                 break
+#             else:
+#                 print("No good")
+#                 print(dns.network_check(PORT))
 
-                break
+#                 break
         
-process()
+# process()
+
+print(dns_bank.bank_get_info(TEMP_VENDOR))
 print(dns.network_check(PORT))
 
